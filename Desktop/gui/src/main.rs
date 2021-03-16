1c28aed8666d790db28e9d54e0cbda24fcf759c7
Karpfly <karpfly@gmail.com>
2021-03-16 21:37:10 +0100

    Added initial main window GUI

--- Desktop/gui/src/main.rs
+++ Desktop/gui/src/main.rs
@@ -1,3 +1,30 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.mackarp.cookbook"), Default::default())
+            .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+}
+
+fn build_ui(application: &gtk::Application) {
+    let glade_src = include_str!("../ui/Main_Window.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    let window: gtk::Window = builder.get_object("MainWindow").unwrap();
+    window.set_application(Some(application));
+
+    window.show_all();
 }
