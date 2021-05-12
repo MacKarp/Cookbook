bb0ffb512c97d4a6ef8bbf1b59d23961348cfa82
Karpfly <karpfly@gmail.com>
2021-05-12 15:01:56 +0200

    Implemented previous stack button

--- Desktop/gui/src/gui_data/connections/buttons/mod.rs
+++ Desktop/gui/src/gui_data/connections/buttons/mod.rs
@@ -1,4 +1,5 @@
 pub mod common;
+pub mod previous_stack;
 pub mod random_drink;
 pub mod random_meal;
 use crate::gui_data::GuiData;
@@ -6,4 +7,5 @@ use crate::gui_data::GuiData;
 pub fn connect(gui_data: &GuiData) {
     random_meal::random_button(&gui_data);
     random_drink::random_button(&gui_data);
+    previous_stack::previous_stack_button(&gui_data);
 }
